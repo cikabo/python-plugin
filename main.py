bcee385e9f695665a0545622e7fb73bcc37bcc55
cikabo <bojan.bjelic@gmail.com>
2026-01-10 23:28:08 +0100

    improve plugin discovery

--- main.py
+++ main.py
@@ -3,34 +3,15 @@
 Main module for the application.
 """
 
-# https://packaging.python.org/en/latest/guides/creating-and-discovering-plugins/
-
 import sys
-import importlib
-import pkgutil
-
-import plugins
 from users import User
-
-
-def iter_namespace(ns_pkg):
-    # Specifying the second argument (prefix) to iter_modules makes the
-    # returned name an absolute name instead of a relative one. This allows
-    # import_module to work without having to do additional modification to
-    # the name.
-    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
-
-discovered_plugins = {
-    name: importlib.import_module(name)
-    for finder, name, ispkg
-    in iter_namespace(plugins)
-}
+from plugin_discovery import discovered_plugins
 
 def main():
     """
     Main entry point of the application.
     """
-    print("Hello, World!")
+
     print("Discovered plugins:", list(discovered_plugins.keys()))
 
     User_instance = User("john_doe", "john_doe@example.com")
---
+++ plugin_discovery.py
@@ -0,0 +1,19 @@
+# https://packaging.python.org/en/latest/guides/creating-and-discovering-plugins/
+
+import importlib
+import pkgutil
+
+import plugins
+
+def iter_namespace(ns_pkg):
+    # Specifying the second argument (prefix) to iter_modules makes the
+    # returned name an absolute name instead of a relative one. This allows
+    # import_module to work without having to do additional modification to
+    # the name.
+    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
+
+discovered_plugins = {
+    name: importlib.import_module(name)
+    for finder, name, ispkg
+    in iter_namespace(plugins)
+}
