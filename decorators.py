ed294109eb6dafe51275fc90483b7780acaa47e7
cikabo <bojan.bjelic@gmail.com>
2026-01-11 16:50:17 +0100

    feat(func_decorator): detailed discovery and selective execution

--- decorators.py
+++ decorators.py
@@ -6,9 +6,23 @@ from plugin_discovery import discovered_plugins
 
 def func_decorator(orig_name, orig_func):
     def decorator(*args, **kwargs):
-         print(f"--> Decorating wrapper called on {orig_name} method {orig_func.__name__}")
-         result = orig_func(*args, **kwargs)
-         return result
+        print(f"-> Decorator wrapper called on {orig_name} method {orig_func.__name__}")
+        for plugin_name, plugin_module in discovered_plugins.items():
+            plugin_class = getattr(plugin_module, orig_name, None)
+            if plugin_class is None:
+                continue
+            plugin_method = getattr(plugin_class(args[0]), orig_func.__name__, None)
+            if plugin_method is None:
+                continue
+            print(f"---> Calling plugin {plugin_name} method {plugin_method.__name__}")
+            if orig_func.__name__ == "__init__":
+                plugin_method(args[0])
+            else:
+                result = plugin_method(*args, **kwargs)
+        if not 'result' in locals():
+            print(f"---> Calling original method {orig_func.__name__}")
+            result = orig_func(*args, **kwargs)
+        return result
     return decorator
 
 def pluggable(cls):
